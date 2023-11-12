ceb92ff7a7f2fb591a56701f536ee9730f9b9559
takenaka <https://github.com/TakenokoTech>
2023-11-12 17:46:59 +0900

    add ios

---
+++ ios/ncache/ncacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  ncacheApp.swift
+//  ncache
+//
+//  Created by Takenoko on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ncacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
